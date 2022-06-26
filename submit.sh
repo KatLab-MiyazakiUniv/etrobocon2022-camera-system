a46858ba9df450f0de18c0328f0e653728b6605f
Taki <javajavawookjavawook@gmail.com>
2022-06-26 20:53:46 +0900

    add: 走行体への通信スクリプトを追加

---
+++ submit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 引数チェック
+if [ $# -ne 1 ]; then
+  echo "Please input 1 or 2 robot id."
+  exit 1
+fi
+
+# データ送信先機体を設定
+if [ $1 -eq 1 ]; then
+    SSH_TARGET="katlab@192.168.11.16"
+elif [ $1 -eq 2 ]; then
+    SSH_TARGET="katlab@192.168.11.17"
+else
+    echo "Missing robot id."
+    exit 1
+fi
+
+# 送信データディレクトリ
+DATA_DIRECTORY="transfer_data"
+SSH_KEY_PATH="~/.ssh/id_etrobocon2022"
+
+if [ -d ${DATA_DIRECTORY} ]; then
+    # 送信データディレクトリを機体に送信
+    scp -i ${SSH_KEY_PATH} -r ${DATA_DIRECTORY} ${SSH_TARGET}:~/work/RasPike/sdk/workspace/etrobocon2022/
+    echo "Completed ${DATA_DIRECTORY} submission."
+else
+    echo "\"${DATA_DIRECTORY}\" does not exist."
+fi
