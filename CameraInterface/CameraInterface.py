5e66fdea9ae9ecffbf797c1f5a67d8c7c9917afd
kawano <kawakawaichi1001@gmail.com>
2022-07-01 02:28:21 +0900

    add : #39 カメラインターフェースを作成

---
+++ CameraInterface/CameraInterface.py
@@ -0,0 +1,40 @@
+##
+# @file CameraInterface.py
+# @brief カメラから画像を取得、保存を行う
+# @author kawano
+
+import sys
+import cv2
+
+# カメラ仲介クラス
+class CameraInterface:
+    ##
+    # @brief コンストラクタ
+    # @param cameraId カメラ番号
+
+    def __init__(self, cameraId):
+        # 画像取得するカメラを選択する（引数はカメラ番号）
+        self.camera = cv2.VideoCapture(cameraId)
+
+    ##
+    # @brief カメラから画像を取得、保存を行う
+    def getFrame(self):
+        # successed   : 画像が取得が成功したか(True or False)
+        # frame : 画像
+        successed, frame = self.camera.read()
+        if successed:
+            # 画像の保存
+            cv2.imwrite("course.png", frame)
+        else:
+            print("画像を取得できませんでした")
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 2:
+        cameraInterface = CameraInterface(int(args[1])) 
+        cameraInterface.getFrame()
+
+    else:
+        print("Warning:コマンドライン引数にカメラIDを入力してください") 
+    
---
+++ CameraInterface/CheckConnectCamera.py
@@ -0,0 +1,33 @@
+##
+# @file CheckConnectCamera.py
+# @brief 接続されているカメラ番号の確認
+# @author kawano
+
+import cv2
+import sys
+
+##    
+# @brief カメラが接続されているかどうかを一覧表示
+def checkCameraConnection():
+    print('接続されているカメラの番号を調べています...')
+    trueCameras = []  # 空の配列を用意
+
+    # カメラ番号を0～9まで変えて、COM_PORTに認識されているカメラを探す
+    for cameraNumber in range(0, 10):
+        try:
+            camera = cv2.VideoCapture(cameraNumber)
+            successed, frame = camera.read()
+        except:
+            successed = False
+        if successed:
+            trueCameras.append(cameraNumber)
+            print("カメラ番号->", cameraNumber, "接続済")
+
+        else:
+            print("カメラ番号->", cameraNumber, "未接続")
+    print("接続されているカメラは", len(trueCameras), "台です。")
+    print("カメラのインデックスは", trueCameras,"です。")
+    sys.exit("カメラ番号を調べ終わりました。")
+
+if __name__ == "__main__":
+    checkCameraConnection()
