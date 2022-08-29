1d58729940a9b0a7571d218e0fd77b08952dbb3e
kodama0720 <bnuy1221@gmail.com>
2022-08-29 10:46:08 +0900

    ゲームエリア情報クラスの追加

---
+++ camera_system/kawano_work/game_info.py
@@ -0,0 +1,29 @@
+
+class GameInfo:
+    """ゲームエリア情報を保持するクラス.
+
+    Attributes:
+        block_list (ndarray): ブロック
+        node_list (ndarray): ノード
+        color_list (ndarray): ベースエリアの色
+    """
+        block_list = []
+        node_list = []
+        color_list = []
+
+    def get_candidate_node:
+    """設置先ノードの候補を取得する関数."""
+
+    def get_no_transported_block:
+    """運搬していないブロックがあるブロック置き場を取得する関数."""
+
+    def get_bonus_color:
+    """ボーナスブロックの色を取得する関数."""
+
+    def get_no_entry_coord:
+    """走行禁止座標を取得する関数."""
+
+    def get_no_rotate_direction:
+    """回頭禁止方向を取得する関数."""
+
+
