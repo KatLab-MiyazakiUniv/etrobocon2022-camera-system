f6932b36fce727aa1fbc619b91c2f7ae7e28af4e
kodama0720 <bnuy1221@gmail.com>
2022-08-31 02:55:49 +0900

    fix:エラーの修正

--- camera_system/kawano_work/game_info.py
+++ camera_system/kawano_work/game_info.py
@@ -1,13 +1,16 @@
 from typing import List, Tuple, Dict
+import robot
 
 
 class GameInfo:
     """ゲームエリア情報を保持するクラス.
 
     Attributes:
-        block_list (List[int, int]): ブロック([ブロックID, 色ID])
-        node_dict (Dict[Tuple(int, int), int]): ノード([座標,ブロックID])
-        base_color_dict (Dict(int)): ベースエリアの色ID(赤、黄、緑、青)
+        __block_id_list (int): ブロック(色ID)
+        __base_id_list (int): ベースブロック(色ID)
+        __end_id (int): ボーナスブロック(色ID)
+        __node_dict (Dict[Tuple(int, int), int]): ノード([座標,ブロックID])
+        __base_color_dict (Dict(int)): ベースエリアの色ID(赤、黄、緑、青)
     """
 
     __block_id_list = []
@@ -24,31 +27,6 @@ class GameInfo:
     }
     __base_color_dict = {0: "東", 1: "南", 2: "西", 3: "北"}
 
-    def color_block_setter(block_id_list: List[int]) -> None:
-        """ block_id_listのsetter.
-
-        Args:
-            block_id_list: カラーブロックの色idの配列
-
-        """
-        GameInfo.__block_id_list = block_id_list
-
-    def base_color_block_setter(base_id_list: List[int]) -> None:
-        """ block_id_listのsetter.
-
-        Args:
-            block_id_list: ベースサークルブロックの色idの配列
-        """
-        GameInfo.__base_id_list = base_id_list
-
-    def bonus_block_setter(end_id: int) -> None:
-        """ block_id_listのsetter.
-
-        Args:
-            block_id_list: ボーナスブロックの色id
-        """
-        GameInfo.__end_id = end_id
-
     def get_candidate_node(self, color: int) -> List[Tuple[int, int]]:
         """設置先ノードの候補を取得する関数.
 
@@ -64,26 +42,26 @@ class GameInfo:
         west_cand_list = [(0, 2), (0, 3), (0, 4)]  # 西の候補ノードになりうる座標リスト
         north_cand_list = [(2, 0), (3, 0), (4, 0)]  # 北の候補ノードになりうる座標リスト
 
-        if base_color_dict[color] == "東":
+        if GameInfo.__base_color_dict[color] == "東":
             for cand in east_cand_list:
                 if GameInfo.__node_dict[cand] == -1:  # 対象のノードに既に設置済みのブロックがなければ候補に入れる
-                    cand_list.append[cand]
-        elif base_color_dict[color] == "南":
+                    cand_list.append(cand)
+        elif GameInfo.__base_color_dict[color] == "南":
             for cand in south_cand_list:
                 if GameInfo.__node_dict[cand] == -1:
-                    cand_list.append[cand]
-        elif base_color_dict[color] == "西":
+                    cand_list.append(cand)
+        elif GameInfo.__base_color_dict[color] == "西":
             for cand in west_cand_list:
                 if GameInfo.__node_dict[cand] == -1:
-                    cand_list.append[cand]
+                    cand_list.append(cand)
         else:
             for cand in north_cand_list:
                 if GameInfo.__node_dict[cand] == -1:
-                    cand_list.append[cand]
+                    cand_list.append(cand)
 
         return cand_list
 
-    def get_no_transported_block(self) -> List[List(int, int)]:
+    def get_no_transported_block(self):  # -> List[List[int, int]]: TypeHintを書くとエラーになる
         """運搬していないブロックがあるブロック置き場を取得する関数.
 
         Returns:
@@ -91,32 +69,18 @@ class GameInfo:
         """
         no_trans_block_list = []  # 候補ノードの座標リストを格納する
 
-        for node in node_dict:
-            x = node[0][0]
-            y = node[0][1]
-            block_id = node[1]
+        for key, value in GameInfo.__node_dict.items():
+            x = key[0]
+            y = key[1]
+            block_id = value
 
             if block_id != -1:  # ブロックがない座標を省く
                 if x % 2 != 0 and y % 2 != 0:  # xとyが奇数ならば運搬していないブロックと判定
-                    no_trans_block_list += [coord_x, coord_y]
+                    no_trans_block_list.append([x, y])
 
         return no_trans_block_list
 
-    def get_bonus_color(self) -> int:
-        """ボーナスブロックの色を取得する関数.
-
-        Returns:
-            int: ボーナスブロックの色
-        """
-        coord = CameraCoordinateCalibrator()
-        c_changer = ColorChanger()
-        x = coord.end_point[0]
-        y = coord.end_point[1]
-        mode = c_changer.mode_color(x, y, 5, 5)
-
-        return mode
-
-    def get_no_entry_coordinate(self, robot) -> List[List[int, int]]:
+    def get_no_entry_coordinate(self, robot):  # -> List[List[int, int]]: TypeHintを書くとエラーになる
         """走行禁止座標を取得する関数.
 
         Args:
@@ -129,30 +93,30 @@ class GameInfo:
         y = robot.coord[1]
         no_entry_list = []
 
-        if [x+1, y] in GameInfo.node_dict.keys() and GameInfo.node_dict[x+1, y] != -1:  # 東にブロック
+        if (x+1, y) in GameInfo.__node_dict.keys() and GameInfo.__node_dict[x+1, y] != -1:  # 東にブロック
             no_entry_list.append([x+1, y])  # ブロックのある座標
-            if [x+1, y-1] in GameInfo.node_dict.keys():
+            if (x+1, y-1) in GameInfo.__node_dict.keys():
                 no_entry_list.append([x+1, y-1])  # ブロックのある座標の北の座標
-            if [x+1, y+1] in GameInfo.node_dict.keys():
+            if (x+1, y+1) in GameInfo.__node_dict.keys():
                 no_entry_list.append([x+1, y+1])  # ブロックのある座標の南の座標
-        if [x, y+1] in GameInfo.node_dict.keys() and GameInfo.node_dict[x, y+1] != -1:  # 南にブロック
-            no_entry_list.append([x, y+1])
-            if [x-1, y+1] in GameInfo.node_dict.keys():
-                no_entry_list.append([x-1, y+1])
-            if [x-1, y+1] in GameInfo.node_dict.keys():
-                no_entry_list.append([x+1, y+1])
-        if [x-1, y] in GameInfo.node_dict.keys() and GameInfo.node_dict[x-1, y] != -1:  # 西にブロック
-            no_entry_list.append([x-1, y])
-            if [x-1, y-1] in GameInfo.node_dict.keys():
-                no_entry_list.append([x-1, y-1])
-            if [x-1, y+1] in GameInfo.node_dict.keys():
-                no_entry_list.append([x-1, y+1])
-        if [x, y-1] in GameInfo.node_dict.keys() and GameInfo.node_dict[x, y-1] != -1:  # 北にブロック
-            no_entry_list.append([x-1, y])
-            if [x-1, y-1] in GameInfo.node_dict.keys():
-                no_entry_list.append([x-1, y-1])
-            if [x+1, y-1] in GameInfo.node_dict.keys():
-                no_entry_list.append([x+1, y-1])
+        if (x, y+1) in GameInfo.__node_dict.keys() and GameInfo.__node_dict[x, y+1] != -1:  # 南にブロック
+            no_entry_list.append([x, y+1])  # ブロックのある座標
+            if (x-1, y+1) in GameInfo.__node_dict.keys():
+                no_entry_list.append([x-1, y+1])  # ブロックのある座標の西の座標
+            if (x-1, y+1) in GameInfo.__node_dict.keys():
+                no_entry_list.append([x+1, y+1])  # ブロックのある座標の東の座標
+        if (x-1, y) in GameInfo.__node_dict.keys() and GameInfo.__node_dict[x-1, y] != -1:  # 西にブロック
+            no_entry_list.append([x-1, y])  # ブロックのある座標
+            if (x-1, y-1) in GameInfo.__node_dict.keys():
+                no_entry_list.append([x-1, y-1])  # ブロックのある座標の北の座標
+            if (x-1, y+1) in GameInfo.__node_dict.keys():
+                no_entry_list.append([x-1, y+1])  # ブロックのある座標の南の座標
+        if (x, y-1) in GameInfo.__node_dict.keys() and GameInfo.__node_dict[x, y-1] != -1:  # 北にブロック
+            no_entry_list.append([x, y-1])  # ブロックのある座標
+            if (x-1, y-1) in GameInfo.__node_dict.keys():
+                no_entry_list.append([x-1, y-1])  # ブロックのある座標の西の座標
+            if [x+1, y-1] in GameInfo.__node_dict.keys():
+                no_entry_list.append([x+1, y-1])  # ブロックのある座標の東の座標
 
         return no_entry_list
 
@@ -170,27 +134,55 @@ class GameInfo:
         direct = robot.direct
         no_rotate_list = []
 
-        if [x+1, y] in GameInfo.node_dict.keys() and GameInfo.node_dict[x+1, y] != -1:  # 東にブロック
+        # 東にブロック
+        if (x+1, y) in GameInfo.__node_dict.keys() and GameInfo.__node_dict[x+1, y] != -1:
             no_rotate_list += [6]  # 西を回頭禁止方向に追加
-        if [x, y+1] in GameInfo.node_dict.keys() and GameInfo.node_dict[x, y+1] != -1:  # 南にブロック
+        # 南にブロック
+        if (x, y+1) in GameInfo.__node_dict.keys() and GameInfo.__node_dict[x, y+1] != -1:
             no_rotate_list += [0]  # 北を回頭禁止方向に追加
-        if [x-1, y] in GameInfo.node_dict.keys() and GameInfo.node_dict[x-1, y] != -1:  # 西にブロック
+        # 西にブロック
+        if (x-1, y) in GameInfo.__node_dict.keys() and GameInfo.__node_dict[x-1, y] != -1:
             no_rotate_list += [2]  # 東を回頭禁止方向に追加
-        if [x, y-1] in GameInfo.node_dict.keys() and GameInfo.node_dict[x, y-1] != -1:  # 北にブロック
+        # 北にブロック
+        if (x, y-1) in GameInfo.__node_dict.keys() and GameInfo.__node_dict[x, y-1] != -1:
             no_rotate_list += [4]  # 南を回頭禁止方向に追加
 
         # 回頭禁止方向が2つ以上ある場合、それらの間にある方向も回頭禁止
-        if min(no_rotate_list) < direct and direct < max(no_rotate_list):
-            for i in range(min(no_rotate_list)):
-                no_rotate_list += [i]
-            for i in range(direct+1, max(no_rotate_list)):
-                no_rotate_list += [i]
-        else:
-            for i in range(min(no_rotate_list)+1, max(no_rotate_list)):
-                no_rotate_list += [i]
+        if len(no_rotate_list) >= 2:
+            if min(no_rotate_list) < direct and direct < max(no_rotate_list):
+                for i in range(min(no_rotate_list)):
+                    no_rotate_list += [i]
+                for i in range(max(no_rotate_list)+1, 8):
+                    no_rotate_list += [i]
+            else:
+                for i in range(min(no_rotate_list)+1, max(no_rotate_list)):
+                    no_rotate_list += [i]
 
         return no_rotate_list
 
-    if __name__ == "__main__":
-        # インスタンス化
-        info = GameInfo()
+
+if __name__ == "__main__":
+    # インスタンス化
+    info = GameInfo()
+    robo = robot.Robot()
+    robo.coord = [2, 1]
+    robo.direct = 4
+    color = 0  # 赤
+    block = [0, 0, 1, 1, 2, 2, 3, 3]
+    base = [0, 1, 2, 3]
+    end = 0
+
+    info.__block_id_list = block
+    info.__base_id_list = base
+    info.__end_id = end
+    print(info.__block_id_list)
+    print(info.__base_id_list)
+    print(info.__end_id)
+    print("候補ノード取得")
+    print(info.get_candidate_node(color))
+    print("未運搬のブロックがあるブロック置き場")
+    print(info.get_no_transported_block())
+    print("走行禁止座標")
+    print(info.get_no_entry_coordinate(robo))
+    print("回頭禁止方向")
+    print(info.get_no_rotate_direction(robo))
