40fb122929aff9b0fa91671f6d3ecd932344e032
mutotaka0426 <hm17058@student.miyazaki-u.ac.jp>
2022-09-07 21:45:49 +0900

    add: 運搬ブロック決定クラスを追加

---
+++ camera_system/block_selector.py
@@ -0,0 +1,74 @@
+"""運搬ブロック決定モジュール.
+
+次に運搬するブロックを決定する
+@author mutotaka0426
+"""
+
+from game_area_info import GameAreaInfo
+from robot import Robot
+from coordinate import Coordinate
+from node import Node
+
+
+class BlockSelector:
+    """運搬ブロック決定クラス."""
+
+    def select_block(self, robot: Robot) -> Node:
+        """ゲームエリア情報を基に次に運搬するブロックを返す.
+
+        Args:
+            robot: 現在の走行体の状態
+
+        Returns:
+            Node: 次に運搬するブロックを持つノード
+        """
+        cand_nodes = GameAreaInfo.get_no_transported_block()  # ブロックID順に候補ブロックを持つノードを格納
+
+        # 優先順位1: 走行体からマンハッタン距離が近いブロック
+        if(len(cand_nodes) > 1):  # 候補が複数ある場合
+            # 最も小さい(走行体→ブロック)のマンハッタン距離を求める
+            min_distance = min([self.__calculate_distance(robot.coord, node.coord)
+                               for node in cand_nodes])
+            # マンハッタン距離が最も小さいブロックを持つノードに絞り込む
+            cand_nodes = [node for node in cand_nodes if min_distance ==
+                          self.__calculate_distance(robot.coord, node.coord)]
+
+        # 優先順位2: ダブルアップボーナスとなるブロック
+        if(len(cand_nodes) > 1):  # 候補が複数ある場合
+            # ボーナスブロックと同じ色のブロックを持つノードに絞り込む
+            double_up_nodes = [
+                node for node in cand_nodes
+                if GameAreaInfo.block_color_list[node.block_id] == GameAreaInfo.bonus_color]
+            # ボーナスブロックと同じ色のブロックがない場合はcand_nodesはそのまま
+            if double_up_nodes != []:
+                cand_nodes = double_up_nodes
+
+        # 優先順位3: 設置先までのマンハッタン距離が小さいブロック
+        if(len(cand_nodes) > 1):  # 候補が複数ある場合
+            # 最も小さい(ブロック→設置先)のマンハッタン距離を求める
+            min_distance = min([self.__calculate_distance(node.coord, destination.coord)
+                                for node in cand_nodes
+                                for destination in GameAreaInfo.get_candidate_node(
+                                    GameAreaInfo.block_color_list[node.block_id])])
+            # 設置先のマンハッタン距離が最も小さいブロックを持つノードに絞り込む
+            cand_nodes = [node for node in cand_nodes
+                          for destination
+                          in GameAreaInfo.get_candidate_node(
+                              GameAreaInfo.block_color_list[node.block_id])
+                          if min_distance == self.__calculate_distance(
+                              node.coord, destination.coord)]
+
+        # 優先順位4：ブロックIDの数字が小さいブロック
+        return cand_nodes[0]
+
+    def __calculate_distance(self, coord1: Coordinate, coord2: Coordinate) -> int:
+        """2点座標のマンハッタン距離を求める.
+
+        Args:
+            coord1: 座標1
+            coord2: 座標2
+
+        Returns:
+            int: 座標1と座標2のマンハッタン距離
+        """
+        return abs(coord1.x-coord2.x) + abs(coord1.y-coord2.y)
---
+++ tests/test_block_selector.py
@@ -0,0 +1,120 @@
+"""BlockSelectorクラスのテストコードを記述するモジュール.
+
+@author: mutotaka0426
+"""
+
+
+import unittest
+
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parent.parent))
+sys.path.append(str(Path(__file__).parent.parent / "camera_system"))
+from camera_system.block_selector import BlockSelector  # noqa
+from game_area_info import GameAreaInfo  # noqa
+from robot import Robot, Direction  # noqa
+from coordinate import Coordinate  # noqa
+from color_changer import Color  # noqa
+from node import Node  # noqa
+
+
+class TestBlockSelector(unittest.TestCase):
+    def test_select_block_priority1(self):
+        """優先順位1で決定するパターン."""
+        robot = Robot(Coordinate(6, 6), Direction.N.value)
+        GameAreaInfo.block_color_list = [
+            Color.RED, Color.RED, Color.YELLOW,
+            Color.YELLOW, Color.GREEN,
+            Color.GREEN, Color.BLUE, Color.BLUE
+        ]
+        GameAreaInfo.base_color_list = [
+            Color.RED, Color.YELLOW,
+            Color.GREEN, Color.BLUE
+        ]
+        GameAreaInfo.bonus_color = Color.RED
+
+        block_selector = BlockSelector()
+        actual = block_selector.select_block(robot)
+        actual_color = GameAreaInfo.block_color_list[actual.block_id]
+        expected = Node(7, Coordinate(5, 5))  # 右下のブロック置き場
+        expected_color = Color.BLUE
+
+        self.assertEqual(expected.block_id, actual.block_id)  # ブロックIDのテスト
+        self.assertEqual(expected.coord, actual.coord)  # 座標のテスト
+        self.assertEqual(expected.node_type, actual.node_type)  # ノードの型のテスト
+        self.assertEqual(expected_color, actual_color)  # ブロックの色のテスト
+
+    def test_select_block_priority2(self):
+        """優先順位2で決定するパターン."""
+        robot = Robot(Coordinate(2, 2), Direction.N.value)
+        GameAreaInfo.block_color_list = [
+            Color.RED, Color.GREEN, Color.YELLOW,
+            Color.YELLOW, Color.RED,
+            Color.GREEN, Color.BLUE, Color.BLUE
+        ]
+        GameAreaInfo.base_color_list = [
+            Color.RED, Color.YELLOW,
+            Color.GREEN, Color.BLUE
+        ]
+        GameAreaInfo.bonus_color = Color.GREEN
+
+        block_selector = BlockSelector()
+        actual = block_selector.select_block(robot)
+        actual_color = GameAreaInfo.block_color_list[actual.block_id]
+        expected = Node(1, Coordinate(3, 1))  # 真ん中上のブロック置き場
+        expected_color = Color.GREEN
+
+        self.assertEqual(expected.block_id, actual.block_id)  # ブロックIDのテスト
+        self.assertEqual(expected.coord, actual.coord)  # 座標のテスト
+        self.assertEqual(expected.node_type, actual.node_type)  # ノードの型のテスト
+        self.assertEqual(expected_color, actual_color)  # ブロックの色のテスト
+
+    def test_select_block_priority3(self):
+        """優先順位3で決定するパターン."""
+        robot = Robot(Coordinate(4, 4), Direction.N.value)
+        GameAreaInfo.block_color_list = [
+            Color.RED, Color.GREEN, Color.YELLOW,
+            Color.YELLOW, Color.BLUE,
+            Color.GREEN, Color.BLUE, Color.RED
+        ]
+        GameAreaInfo.base_color_list = [
+            Color.RED, Color.YELLOW,
+            Color.GREEN, Color.BLUE
+        ]
+        GameAreaInfo.bonus_color = Color.GREEN
+
+        block_selector = BlockSelector()
+        actual = block_selector.select_block(robot)
+        actual_color = GameAreaInfo.block_color_list[actual.block_id]
+        expected = Node(7, Coordinate(5, 5))  # 右下のブロック置き場
+        expected_color = Color.RED
+
+        self.assertEqual(expected.block_id, actual.block_id)  # ブロックIDのテスト
+        self.assertEqual(expected.coord, actual.coord)  # 座標のテスト
+        self.assertEqual(expected.node_type, actual.node_type)  # ノードの型のテスト
+        self.assertEqual(expected_color, actual_color)  # ブロックの色のテスト
+
+    def test_select_block_priority4(self):
+        """優先順位4で決定するパターン."""
+        robot = Robot(Coordinate(4, 4), Direction.N.value)
+        GameAreaInfo.block_color_list = [
+            Color.RED, Color.GREEN, Color.YELLOW,
+            Color.RED, Color.BLUE,
+            Color.GREEN, Color.YELLOW, Color.BLUE
+        ]
+        GameAreaInfo.base_color_list = [
+            Color.RED, Color.GREEN,
+            Color.YELLOW, Color.BLUE
+        ]
+        GameAreaInfo.bonus_color = Color.GREEN
+
+        block_selector = BlockSelector()
+        actual = block_selector.select_block(robot)
+        actual_color = GameAreaInfo.block_color_list[actual.block_id]
+        expected = Node(4, Coordinate(5, 3))  # 右真ん中のブロック置き場
+        expected_color = Color.BLUE
+
+        self.assertEqual(expected.block_id, actual.block_id)  # ブロックIDのテスト
+        self.assertEqual(expected.coord, actual.coord)  # 座標のテスト
+        self.assertEqual(expected.node_type, actual.node_type)  # ノードの型のテスト
+        self.assertEqual(expected_color, actual_color)  # ブロックの色のテスト
