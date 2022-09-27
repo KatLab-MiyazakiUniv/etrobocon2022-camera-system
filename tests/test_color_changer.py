227ed431dd41fa3d08ef76d437d2d5f9960632e7
kawano <kawakawaichi1001@gmail.com>
2022-09-28 07:33:01 +0900

    modify:#53 コメントの修正

--- tests/test_color_changer.py
+++ tests/test_color_changer.py
@@ -25,13 +25,13 @@ class TestColorChanger(unittest.TestCase):
         actual_exist = os.path.exists(save_path)  # 画像が生成されているか確認(bool)
         self.assertEqual(expected_exist, actual_exist)
 
-        # search_color関数のテスト
+        # search_color関数のテスト(領域に白しかなかった場合)
         search_area_size = 21
         actual_color_uniqs, actual_color_pixel_sum = cc.search_color(
-            422, 387, search_area_size, search_area_size)  # 領域が白のみ
+            422, 387, search_area_size, search_area_size)
 
         expected_color_uniqs = np.array([1, 2, 3, 4])  # 正解のカラーID
-        expected_color_pixel_sum = np.full(4, search_area_size**2//4)  # 正解のカラーID
+        expected_color_pixel_sum = np.full(4, search_area_size**2//4)  # 正解のピクセル数
 
         self.assertEqual(all(expected_color_uniqs), all(actual_color_uniqs))
         self.assertEqual(all(expected_color_pixel_sum), all(actual_color_pixel_sum))
